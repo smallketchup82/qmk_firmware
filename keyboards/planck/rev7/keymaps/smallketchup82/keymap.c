5642cf5431fa6503ec4fcb4c83234250ec0abee2
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2024-04-18 12:57:57 -0400

    Add custom keymap

---
+++ keyboards/planck/rev7/keymaps/smallketchup82/keymap.c
@@ -0,0 +1,46 @@
+/* Copyright 2015-2023 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum planck_layers { _QWERTY, _SYMBOLS, _NUMBERS, _SETTINGS };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[_QWERTY] = LAYOUT_planck_1x2uC(
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
+        KC_NO, KC_LCTL, KC_LALT, KC_LGUI, TT(_NUMBERS), KC_SPC, TT(_SYMBOLS), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+        ),
+	[_SYMBOLS] = LAYOUT_planck_1x2uC(
+        KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_SETTINGS), KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+        ),
+	[_NUMBERS] = LAYOUT_planck_1x2uC(
+        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+        KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_HOME, KC_END, KC_PGDN, KC_PGUP, KC_INS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LT(_SETTINGS,KC_MPLY), KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+        ),
+	[_SETTINGS] = LAYOUT_planck_1x2uC(
+        KC_TRNS, QK_BOOT, DB_TOGG, BL_TOGG, BL_STEP, BL_BRTG, BL_UP, BL_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, EE_CLR, KC_TRNS, KC_TRNS, KC_TRNS, AG_NORM, AG_SWAP, KC_TRNS, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_U, KC_WH_D, KC_WH_L, KC_WH_R,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_MS_D, KC_MS_L, KC_MS_R
+        )
+};
