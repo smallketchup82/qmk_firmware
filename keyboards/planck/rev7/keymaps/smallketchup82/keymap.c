4e2a55c8b7bda0b658f23a5382bb84b65c5719bf
smallketchup82 <ketchup@smallketchup.ca>
2024-07-04 16:31:06 -0400

    Move mousemode toggle to tap toggle

--- keyboards/planck/rev7/keymaps/smallketchup82/keymap.c
+++ keyboards/planck/rev7/keymaps/smallketchup82/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,         KC_G,   KC_H,   KC_J,         KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_ENTER,
-        TG(_MOUSE), KC_LCTL, KC_LALT, KC_LGUI, MO(_SYMBOLS), KC_SPC, KC_SPC, MO(_NUMBERS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        TT(_MOUSE), KC_LCTL, KC_LALT, KC_LGUI, MO(_SYMBOLS), KC_SPC, KC_SPC, MO(_NUMBERS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
         ),
 	[_SYMBOLS] = LAYOUT_planck_grid(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,        KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         ),
-        [_MOUSE] = LAYOUT_planck_grid(
+  [_MOUSE] = LAYOUT_planck_grid(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_WH_D, KC_WH_U,
